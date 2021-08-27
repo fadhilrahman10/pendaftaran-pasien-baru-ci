821bc4cd11a99a685bb4879d33007c4ed78bf83c
fadhil rahman <fadhilrahman920@gmail.com>
2021-08-27 23:47:14 +0700

    memperbaiki db

--- klinik.sql
+++ klinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Agu 2021 pada 20.49
+-- Waktu pembuatan: 27 Agu 2021 pada 18.46
 -- Versi server: 10.4.20-MariaDB
 -- Versi PHP: 7.4.22
 
@@ -42,6 +42,32 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`no_admin`, `nama`, `email`, `password`, `status`) VALUES
 (1, 'Rowen Rahmat Fauzi', 'admin@gmail.com', '12345', 'master');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `nrm` char(10) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tempat_lahir` varchar(255) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` set('laki-laki','perempuan') NOT NULL,
+  `profesi` varchar(255) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `agama` varchar(100) NOT NULL,
+  `tanggal_dibuat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`nrm`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `profesi`, `no_hp`, `alamat`, `agama`, `tanggal_dibuat`) VALUES
+('NRM-001', 'Rowen Rahmat Fauzi', 'Medan', '1988-05-17', 'laki-laki', 'Pedagang', '081243344342', '-', 'Islam', '2021-08-27 16:46:10');
+
 --
 -- Indexes for dumped tables
 --
@@ -51,6 +77,12 @@ INSERT INTO `admin` (`no_admin`, `nama`, `email`, `password`, `status`) VALUES
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`no_admin`);
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`nrm`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
